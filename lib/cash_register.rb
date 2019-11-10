61030d3826a616d39318d20cdf1795fae727cc1c
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-10 22:41:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,8 +3,6 @@ require 'pry'
 class CashRegister
   attr_accessor :total, :cash_register, :cash_register_with_discount, :last_transaction
 
-
-
   def initialize(discount=0)
     @total = 0
     @discount = discount
